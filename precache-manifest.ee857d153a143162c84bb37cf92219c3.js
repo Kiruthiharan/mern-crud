6e347fdced0051a41704aea2e6b544b04594d320
Kiruthiharan <kiruthiharan@gmail.com>
2019-06-20 20:42:42 +0530

    Updates

---
+++ precache-manifest.ee857d153a143162c84bb37cf92219c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e5e585d13bd9f15f64002e08744aeed",
+    "url": "/Kiruthiharan/mern-crud.git/index.html"
+  },
+  {
+    "revision": "77f328ed5772e2a0202d",
+    "url": "/Kiruthiharan/mern-crud.git/static/css/main.f9c67ae1.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/Kiruthiharan/mern-crud.git/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "77f328ed5772e2a0202d",
+    "url": "/Kiruthiharan/mern-crud.git/static/js/main.2cc23178.chunk.js"
+  },
+  {
+    "revision": "b92a8285cb2d34d41954",
+    "url": "/Kiruthiharan/mern-crud.git/static/js/runtime~main.f00f1644.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Kiruthiharan/mern-crud.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
